dea51c84c6f9e2a0377d7af370463f14c0043cd2
Youngpil Yoon <yngpil.yoon@gmail.com>
2024-06-15 15:16:04 +0900

    refactor(hack): rename NFS_EXPORT_PATH to NFS_PATH in setup script

--- hack/nfs-server.sh
+++ hack/nfs-server.sh
@@ -5,9 +5,9 @@
 sudo apt update
 sudo apt install -y nfs-kernel-server
 
-sudo mkdir -p ${NFS_EXPORT_PATH}
-sudo chown nobody:nogroup ${NFS_EXPORT_PATH}
-sudo chmod g+rwxs ${NFS_EXPORT_PATH}
+sudo mkdir -p ${NFS_PATH}
+sudo chown nobody:nogroup ${NFS_PATH}
+sudo chmod g+rwxs ${NFS_PATH}
 
 echo "/mnt/nfs  ${NODE_SUBNET}(rw,sync,no_subtree_check,insecure,no_root_squash,no_all_squash)" | sudo tee -a /etc/exports
 
